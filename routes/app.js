812059c3e72b6d57ccbb828c58a577ffaa33fd47
paulpellom@gmail.com <paulpellom@gmail.com>
2018-02-22 14:09:29 -0500

    some progress

---
+++ routes/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+
+//get the main confessions from database
+router.get('/confessions', function (req, res) {
+
+    res.send({
+        type: 'GET'
+    });
+})
+
+
+
+router.get('/confessions', function (req, res) {
+
+    res.send({
+        type: 'GET'
+    });
+})
+
+
+router.get('/confessions', function (req, res) {
+
+    res.send({
+        type: 'GET'
+    });
+})
+
+
+router.get('/confessions', function (req, res) {
+
+    res.send({type: 'GET'});
+    })
