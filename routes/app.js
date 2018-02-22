e5d33c084818865280270a1aad8dbaf022ed0a6c
paulpellom@gmail.com <paulpellom@gmail.com>
2018-02-22 14:43:36 -0500

    fuck

--- routes/app.js
+++ routes/app.js
@@ -1,43 +1,8 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
 
-//get the main confessions from database
-router.get('/confessions', function (req, res) {
-
-    res.send({
-        type: 'GET'
-    });
+//Home Page Route
+router.get('/', function (req, res) {
+    res.send('Confessions Home');
 })
 
-
-//Add a new confession to the main DB
-router.post('/confessions', function (req, res) {
-
-    res.send({
-        type: 'POST'
-    });
-})
-
-//Edit a confession thats already been posted
-router.put('/confessions/:id', function (req, res) {
-
-    res.send({
-        type: 'PUT'
-    });
-})
-
-//Delete a Confession from the main DB
-router.delete('/confessions/:id', function (req, res) {
-
-    res.send({type: 'DELETE'});
-    })
-
-
-
-
-
-
-
-
-
-    module.exports = router;
