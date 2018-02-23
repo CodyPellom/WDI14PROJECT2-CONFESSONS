e12ab4b2810304c31ec75165e2658fba8637de63
paulpellom@gmail.com <paulpellom@gmail.com>
2018-02-23 11:15:54 -0500

    morninit

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const methodOverride = require('method-override');
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const app = express()
 
 
 
-mongoose.connect(process.env.MONGODB_URI);
-const db = mongoose.connection;
+mongoose.connect(process.env.MONGODB_URI)
+const db = mongoose.connection
 
 
 db.on('open', () => {
@@ -23,44 +23,44 @@ db.on('error', (err) => {
   console.log(err)
 })
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
 app.use(methodOverride('_method'))
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 const index = require('./controllers/index')
-const confessionsController = require('./controllers/confessionsController')
+const confessionController = require('./controllers/confessionController')
 const userController = require('./controllers/userController')
 
 
 
-app.use('/', index);
-app.use('/confessions' confessionsController);
-app.use('/confessions/:confessionId/users', userController);
+app.use('/', index)
+app.use('/confessions', confessionController)
+app.use('/confessions/:confessionId/users', userController)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+module.exports = app
