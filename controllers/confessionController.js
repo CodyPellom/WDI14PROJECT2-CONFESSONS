9f8bff919e024dec5a1f6cad2d1e3e3c8d065db2
paulpellom@gmail.com <paulpellom@gmail.com>
2018-02-26 09:21:19 -0500

    morning

--- controllers/confessionController.js
+++ controllers/confessionController.js
@@ -13,7 +13,7 @@ router.get('/', (req, res) => {
   // Find the company by route params defined in app.js
   User.findById(req.params.userId).then((user) => {
 
-    // Pass all sodas and the company to a view specifically for showing all sodas
+    
     const confessions = user.confessions
     res.render('confession/index', {
       user: user,
@@ -26,7 +26,7 @@ router.get('/', (req, res) => {
 // GET
 router.get('/new', (req, res) => {
 
-  // We only need to pass the company ID to this new view
+
   res.render('confessions/new', {
     userId: req.params.userId
   })
@@ -36,20 +36,18 @@ router.get('/new', (req, res) => {
 // POST
 router.post('/', (req, res) => {
 
-  // Get company we need to save soda to
+
   User.findById(req.params.userId).then((user) => {
 
-    // THEN once we have the company, take req.body and make a new Soda
+
     const newConfession = new Confession({
       name: req.body.name,
       price: req.body.price
       
     })
 
-    // Push Soda to company.sodas
     user.confessions.push(newConfession)
 
-    // Save Company
     return user.save()
   }).then((updatedUser) => {
 
@@ -62,13 +60,12 @@ router.post('/', (req, res) => {
 // SHOW
 router.get('/:id', (req, res) => {
 
-  // Find company from companyId route param
+  
   User.findById(req.params.userId).then((user) => {
 
-    // Use the .id method to extract a single soda from company.sodas
     const confession = user.confessions.id(req.params.id)
 
-    // connect it to a soda/show view
+  
     res.render('confession/show', {
       userId: req.params.userId,
       confession: confession
@@ -80,8 +77,7 @@ router.get('/:id', (req, res) => {
 // GET
 router.get('/:id/edit', (req, res) => {
 
-  // Make sure to take a look at the soda/edit file. It will show you a lot concerning how 
-  // to connect the initial values to this edit page
+ 
   User.findById(req.params.userId).then((user) => {
     const confession = user.confessions.id(req.params.id)
     res.render('confession/edit', {
@@ -96,14 +92,13 @@ router.get('/:id/edit', (req, res) => {
 router.patch('/:id', (req, res) => {
   User.findById(req.params.userId).then((user) => {
 
-    // We don't have a nice method like findByIdAndUpdate here
-    // so instead we need to manually change the sodas values
+    
     const confession = user.confessions.id(req.params.id)
     confession.name = req.body.name
     confession.price = req.body.price
  
 
-    // Then Save the company
+    
     return user.save()
   }).then((updatedUser) => {
     res.redirect(`/users/${updatedUser._id}/confessions/${req.params.id}`)
--- controllers/userController.js
+++ controllers/userController.js
@@ -6,21 +6,21 @@ const User = require('../models/user')
 // GET
 router.get('/', (req, res) => {
 
-  // Get All Companies
+  
   User.find().then((users) => {
 
-    // Send all the companies to the hbs file called index in the views/company directory
+   
     res.render('user/index', {
       users: users
     })
   })
 })
-//user = company; confession = soda;
+
 // NEW
 // GET
 router.get('/new', (req, res) => {
 
-  // Just render a view, we don't need to inject any data from our server here
+ re
   res.render('user/new')
 })
 
@@ -28,19 +28,16 @@ router.get('/new', (req, res) => {
 // POST
 router.post('/', (req, res) => {
 
-  // Create a new company and make sure we are ONLY looking at the 
-  // pieces of req.body that we need in order to save to the DB
-  // Data from req.body is coming from the HTML form
+  
   const newUser = new User({
     name: req.body.name,
     location: req.body.location
   })
 
-  // Save the new company
+ 
   newUser.save().then((savedUser) => {
 
-    // THEN redirect to the new companies page
-    // Remember POST/PUT/PATCH/DELETE routes should not render or send anything
+   
     res.redirect(`/users/${savedUser._id}`)
   })
 })
@@ -50,10 +47,10 @@ router.post('/', (req, res) => {
 // GET
 router.get('/:id', (req, res) => {
 
-  // Find a single company
+ 
   User.findById(req.params.id).then((user) => {
 
-    // THEN render that into a handlebars view and pass the company from our db into hbs
+
     res.render('user/show', {
       user: user
     })
@@ -64,10 +61,10 @@ router.get('/:id', (req, res) => {
 // GET
 router.get('/:id/edit', (req, res) => {
 
-  // Find a single company using the route params above
+  
   User.findById(req.params.id).then((user) => {
 
-    // THEN render that and id into a handlebars view and pass the company from our db into hbs
+ 
     res.render('user/edit', {
       id: req.params.id,
       user: user
@@ -79,15 +76,13 @@ router.get('/:id/edit', (req, res) => {
 // PUT/PATCH
 router.patch('/:id', (req, res) => {
 
-  // Use the route params and form data to update the Company
   User.findByIdAndUpdate(req.params.id, {
     name: req.body.name,
     location: req.body.location
 
-    // Make sure you add thie { new: true } flag, else your data may not refresh right away
   }, { new: true }).then((updatedUser) => {
 
-    // Redirect to the show page once it successfully updates
+   
     res.redirect(`/users/${updatedUser._id}`)
   })
 })
@@ -96,7 +91,6 @@ router.patch('/:id', (req, res) => {
 // DELETE
 router.delete('/:id', (req, res) => {
 
-  // Use the params id to find and remove the Company
   User.findByIdAndRemove(req.params.id).then(() => {
     res.redirect(`/users`)
   })
